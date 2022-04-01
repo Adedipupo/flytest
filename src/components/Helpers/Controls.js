38939dc465c3885dfae84a7e536b89345868880f
Adedipupo <ajokanola@terragonltd.com>
2022-04-01 10:24:01 +0100

    wip

--- src/components/Helpers/Controls.js
+++ src/components/Helpers/Controls.js
@@ -1,5 +1,5 @@
-import Button from './controls/Button';
 import Input from './controls/Input';
+import Button from './controls/Button';
 
 export const Controls = {
     Button,
--- src/components/Helpers/controls/button.js
+++ src/components/Helpers/controls/button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button as MuiButton } from "@mui/material";
-import { makeStyles } from '@material-ui/core';
 
 
 
