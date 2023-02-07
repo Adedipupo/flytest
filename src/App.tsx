8bc04b551d711837fed99e149bcfd19cf3e4a5b1
Adedipupo <ajokanola@terragonltd.com>
2023-02-07 14:56:54 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 
 import './App.css'
-import { ThemeProvider } from '@mui/material'
+import { CssBaseline,Button, ThemeProvider } from '@mui/material'
 import { appTheme } from './themes/theme'
 
 function App() {
 
   return (
     <ThemeProvider theme={appTheme}>
-      <>
-      hhh
-      </>
+     <CssBaseline enableColorScheme />
+
+     <div style={{ margin: "1em" }}>
+        <Button color="primary" variant="contained">
+          Primary
+        </Button>
+        <Button color="secondary" variant="contained">
+          Secondary
+        </Button>
+      </div>
     </ThemeProvider>
   )
 }
