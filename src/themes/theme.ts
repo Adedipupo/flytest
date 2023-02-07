503762ea13c3a1e34b593d7df90d317877387fad
Adedipupo <ajokanola@terragonltd.com>
2023-02-07 10:03:51 +0100

    theme-setup

---
+++ src/themes/theme.ts
@@ -0,0 +1,10 @@
+
+import { blue, pink } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+​
+export const appTheme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+});
