8f0290f4b76aa169b40fb5532b9a05f367d73471
Adedipupo <ajokanola@terragonltd.com>
2023-02-07 10:19:20 +0100

    vite----git add .git add .

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,7 +1,8 @@
-
 import { blue, pink } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
-​
+
+
+
 export const appTheme = createTheme({
   palette: {
     primary: blue,
